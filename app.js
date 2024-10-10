f00a3d41dbfeece3814dd3c198b5ccd0a00cc1e6
SaquibFurniturewala1 <saquibfurniturewala1@gmail.com>
2024-10-10 13:56:34 -0400

    Added Home section and started development on the Plane Scale section

---
+++ app.js
@@ -0,0 +1,41 @@
+let navListItems = document.querySelectorAll("#nav-list li");
+
+function navStyle() {
+  navListItems.forEach(item => {
+    item.style.padding = '5px 20px'
+    item.style.borderRadius = '5px'
+    item.style.transition = 'all 0.3s linear'
+  });
+}
+navStyle();
+
+function navHover() {
+  navListItems.forEach(navH => {
+    navH.addEventListener('mouseover', () => {
+      navH.style.transform = 'scale(1.1)'
+      navH.style.boxShadow = 'var(--shadow)';
+    })
+    navH.addEventListener('mouseout', () => {
+      navH.style.transform = ''
+      navH.style.boxShadow = ''
+    })
+  });
+}
+navHover();
+
+function animation() {
+  gsap.to('.nav-container', {
+    backgroundColor: '#000',
+    duration: 0.5,
+    height: '70px',
+    scrollTrigger: {
+      trigger: '.nav-container',
+      scroller: 'body',
+      start: "top -15%",
+      end: "top -10%",
+      scrub: 1,
+      // markers: true, // Uncomment for debugging
+    }
+  });
+}
+animation();
