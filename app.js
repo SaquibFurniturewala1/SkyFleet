f99f5690e7e7e4eba8519e721552f626556abb83
SaquibFurniturewala1 <saquibfurniturewala1@gmail.com>
2024-10-13 06:40:57 -0400

    Added the LogIn popup section

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+// LogIn/SignUp function
+
+const logIn = document.querySelector('#nav-list') // storing in the const variable
+const container = document.querySelector('.form-container')  // storing in the const variable
+
+function popUp () { // popUp is the funcation name
+  logIn.addEventListener('click', () => {  // added the addEventListener, so when click the form should show up
+    if (container.style.display  = 'flex') { // the formt that's hold everything
+      container.style.display === 'none'
+    } else {
+      container.style.display === 'flex'
+    }
+  })
+}
+popUp ();  // calling the function to work
+
+ function hide () { // hide is the funcation name
+  document.addEventListener('click', (e) => {  // e is the parameter
+    if (!container.contains(e.target) && !logIn.contains(e.target)) {   // if the user click outsite the from box, the box should we hidden or none
+      container.style.display = 'none'
+    }
+  })
+ }
+hide(); // calling the function to work
+
+
 setTimeout(() => {
 
   document.querySelector('.intro').style.display = 'none';
@@ -5,30 +31,30 @@ setTimeout(() => {
 }, 5000); 
 
 
-let navListItems = document.querySelectorAll("#nav-list li");
+// let navListItems = document.querySelectorAll("#nav-list li");
 
-function navStyle() {
-  navListItems.forEach(item => {
-    item.style.padding = '5px 20px'
-    item.style.borderRadius = '5px'
-    item.style.transition = 'all 0.3s linear'
-  });
-}
-navStyle();
+// function navStyle() {
+//   navListItems.forEach(item => {
+//     item.style.padding = '5px 20px'
+//     item.style.borderRadius = '5px'
+//     item.style.transition = 'all 0.3s linear'
+//   });
+// }
+// navStyle();
 
-function navHover() {
-  navListItems.forEach(navH => {
-    navH.addEventListener('mouseover', () => {
-      navH.style.transform = 'scale(1.1)'
-      navH.style.boxShadow = 'var(--shadow)';
-    })
-    navH.addEventListener('mouseout', () => {
-      navH.style.transform = ''
-      navH.style.boxShadow = ''
-    })
-  });
-}
-navHover();
+// function navHover() {
+//   navListItems.forEach(navH => {
+//     navH.addEventListener('mouseover', () => {
+//       navH.style.transform = 'scale(1.1)'
+//       navH.style.boxShadow = 'var(--shadow)';
+//     })
+//     navH.addEventListener('mouseout', () => {
+//       navH.style.transform = ''
+//       navH.style.boxShadow = ''
+//     })
+//   });
+// }
+// navHover();
 
 function animation() {
   gsap.to('.nav-container', {
