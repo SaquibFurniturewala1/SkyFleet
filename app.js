6c64ebd6e56161c22039c4c28d341131b3c525e9
SaquibFurniturewala1 <saquibfurniturewala1@gmail.com>
2024-10-14 11:06:50 -0400

    Updated the signup form

--- app.js
+++ app.js
@@ -1,60 +1,35 @@
-// LogIn/SignUp function
-
-const logIn = document.querySelector('#nav-list') // storing in the const variable
-const container = document.querySelector('.form-container')  // storing in the const variable
-
-function popUp () { // popUp is the funcation name
-  logIn.addEventListener('click', () => {  // added the addEventListener, so when click the form should show up
-    if (container.style.display  = 'flex') { // the formt that's hold everything
-      container.style.display === 'none'
-    } else {
-      container.style.display === 'flex'
-    }
-  })
-}
-popUp ();  // calling the function to work
-
- function hide () { // hide is the funcation name
-  document.addEventListener('click', (e) => {  // e is the parameter
-    if (!container.contains(e.target) && !logIn.contains(e.target)) {   // if the user click outsite the from box, the box should we hidden or none
-      container.style.display = 'none'
-    }
-  })
- }
-hide(); // calling the function to work
-
-
 setTimeout(() => {
-
   document.querySelector('.intro').style.display = 'none';
   document.querySelector('.main-content').style.display = 'block';
 }, 5000); 
 
 
-// let navListItems = document.querySelectorAll("#nav-list li");
+let navListItems = document.querySelectorAll("#nav-list li");
 
-// function navStyle() {
-//   navListItems.forEach(item => {
-//     item.style.padding = '5px 20px'
-//     item.style.borderRadius = '5px'
-//     item.style.transition = 'all 0.3s linear'
-//   });
-// }
-// navStyle();
+function navStyle() {
+  navListItems.forEach(item => {
+    item.style.padding = '5px 20px'
+    item.style.borderRadius = '5px'
+    item.style.transition = 'all 0.3s linear'
+  });
+}
+navStyle();
 
-// function navHover() {
-//   navListItems.forEach(navH => {
-//     navH.addEventListener('mouseover', () => {
-//       navH.style.transform = 'scale(1.1)'
-//       navH.style.boxShadow = 'var(--shadow)';
-//     })
-//     navH.addEventListener('mouseout', () => {
-//       navH.style.transform = ''
-//       navH.style.boxShadow = ''
-//     })
-//   });
-// }
-// navHover();
+
+function navHover() {
+  navListItems.forEach(navH => {
+    navH.addEventListener('mouseover', () => {
+      navH.style.transform = 'scale(1.1)'
+      navH.style.boxShadow = 'var(--shadow)';
+    })
+    navH.addEventListener('mouseout', () => {
+      navH.style.transform = ''
+      navH.style.boxShadow = ''
+    })
+  });
+}
+navHover();
+
 
 function animation() {
   gsap.to('.nav-container', {
@@ -72,3 +47,6 @@ function animation() {
   });
 }
 animation();
+
+
+// LogIn Form JS
